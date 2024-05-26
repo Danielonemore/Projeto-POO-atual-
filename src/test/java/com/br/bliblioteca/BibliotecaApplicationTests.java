c688d0cbd64686efdcfc3a3b3620b2724d524066
Danielonemore <165956975+Danielonemore@users.noreply.github.com>
2024-05-25 21:53:37 -0300

    primeiro commit

---
+++ src/test/java/com/br/bliblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bliblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
